67c33b027bb66e1a19dab178b38dff9355347bd7
DinhHaiDang66 <dangdhhe170239@fpt.edu.vn>
2025-12-12 16:39:31 +0700

    update

--- FE/project/src/constants/categories.ts
+++ FE/project/src/constants/categories.ts
@@ -6,13 +6,4 @@ export const CATEGORIES = [
   { id: 5, name: 'HSK' },
 ] as const;
 
-export const LANGUAGES = [
-  { id: 1, name: 'English', code: 'en' },
-  { id: 2, name: 'Vietnamese', code: 'vi' },
-  { id: 3, name: 'Chinese', code: 'zh' },
-  { id: 4, name: 'Japanese', code: 'ja' },
-  { id: 5, name: 'Korean', code: 'ko' },
-] as const;
-
 export type Category = typeof CATEGORIES[number];
-export type Language = typeof LANGUAGES[number];
